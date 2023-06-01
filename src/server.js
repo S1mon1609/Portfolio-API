e7418581331a7b5f86d30fe1f247072fdbd47259
S1mon1609 <116732154+S1mon1609@users.noreply.github.com>
2023-06-01 14:38:42 +0200

    3 files modified

--- src/server.js
+++ src/server.js
@@ -28,18 +28,151 @@ function validateRecord(record) {
         record.reading && typeof record.reading === "number";
 }
 
-function readRecordsFromFile(){
-  try {
-      const data = fs.readFileSync('src/records.json', 'utf-8');
-      return JSON.parse(data);
-  } catch (err) {
-      console.error('Error reading records from file:', err);
-      return {};
-  }
-}
+app.get('/records', (req, res) => {
+    fs.readFile('src/records.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal Server Error' });
+        return;
+      }
+  
+      try {
+        const records = JSON.parse(data);
+        res.json(records);
+      } catch (parseError) {
+        console.error(parseError);
+        res.status(500).json({ error: 'Error parsing JSON' });
+      }
+    });
+  });
+  
+  app.post('/records', (req, res) => {
+    const newRecord = req.body;
+  
+    if (!validateRecord(newRecord)) {
+      res.status(400).json({ error: 'Ungültiger Datensatz' });
+      return;
+    }
+  
+    fs.readFile('src/records.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal Server Error' });
+        return;
+      }
+  
+      try {
+        const existingRecords = JSON.parse(data);
+  
+        const id = generateId();
+  
+        newRecord.id = id;
+  
+        const recordsArray = Array.isArray(existingRecords) ? existingRecords : [existingRecords];
+  
+        recordsArray.push(newRecord);
+  
+        fs.writeFile('src/records.json', JSON.stringify(recordsArray), 'utf8', (err) => {
+          if (err) {
+            console.error(err);
+            res.status(500).json({ error: 'Internal Server Error' });
+            return;
+          }
+  
+          const header = getRecordUrl(req, id);
+          res.status(201).location(header).json({ success: true });
+        });
+      } catch (parseError) {
+        console.error(parseError);
+        res.status(500).json({ error: 'Error parsing JSON' });
+      }
+    });
+  });
+  
+  app.delete('/records', (req, res) => {
+    fs.writeFile('src/records.json', JSON.stringify([]), 'utf8', (err) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal Server Error' });
+        return;
+      }
+  
+      res.json({ success: true });
+    });
+  });
+  
+  app.listen(3000, () => {
+    console.log('Server listening on port 3000');
+  });
+  app.get('/records/:id', (req, res) => {
+    const recordId = req.params.id;
+  
+    fs.readFile('src/records.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal Server Error' });
+        return;
+      }
+  
+      try {
+        const records = JSON.parse(data);
+        const record = records.find((rec) => rec.id === recordId);
+  
+        if (!record) {
+          res.status(404).json({ error: 'Datensatz nicht gefunden' });
+          return;
+        }
+  
+        res.json(record);
+      } catch (parseError) {
+        console.error(parseError);
+        res.status(500).json({ error: 'Error parsing JSON' });
+      }
+    });
+  });
+  
+  app.delete('/records/:id', (req, res) => {
+    const recordId = req.params.id;
 
-app.get("src/records", req, res =>{
-  console.log("Get-Function")
-  const items = readRecordsFromFile
-  res.status(200).json(items)
-});
+    console.log(recordId);
+  
+    fs.readFile('src/records.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal Server Error' });
+        return;
+      }
+  
+      try {
+        let records = JSON.parse(data);
+
+        console.log(records);
+
+        const recordIndex = records.findIndex((rec) => rec.id == recordId);
+        
+        console.log(recordIndex);
+
+        if (recordIndex === -1) {
+          res.status(404).json({ error: 'Datensatz nicht gefunden' });
+          return;
+        }
+  
+        records.splice(recordIndex, 1);
+  
+        fs.writeFile('src/records.json', JSON.stringify(records), 'utf8', (err) => {
+          if (err) {
+            console.error(err);
+            res.status(500).json({ error: 'Internal Server Error' });
+            return;
+          }
+  
+          res.json({ success: true });
+          res.send();
+        });
+      } catch (parseError) {
+        console.error(parseError);
+        res.status(500).json({ error: 'Error parsing JSON' });
+      }
+    });
+  });
+  
