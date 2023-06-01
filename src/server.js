5479216e65ee216d73acc4b018fcf40817a8a474
S1mon1609 <116732154+S1mon1609@users.noreply.github.com>
2023-06-01 22:27:01 +0200

    3 files modified

--- src/server.js
+++ src/server.js
@@ -38,6 +38,7 @@ app.get('/records', (req, res) => {
   
       try {
         const records = JSON.parse(data);
+        console.log("Yan ist ein kek");
         res.json(records);
       } catch (parseError) {
         console.error(parseError);
@@ -105,30 +106,54 @@ app.get('/records', (req, res) => {
     
     });
   });
-  app.delete('/records/:record-id', (req, res) => {
-    const { recordId } = req.params;
-  
-    console.log(recordId);
+
+  app.get('/records/:id', (req, res) => {
+
+    const id = req.params.id;
 
     fs.readFile('src/records.json', 'utf8', (err, data) => {
       if (err) {
         console.error(err);
-        res.status(500).json({ error: 'Internal Server Error' });
-        return;
+        return res.status(500).json({ error: 'Server-Fehler.' });
       }
   
-      try {
-        let records = JSON.parse(data);
-  
-        if (records.items.find(item => item.id === recordId)) {
-          records.items = records.items.filter(item => item.id !== recordId);
-          saveRecordsToFile(records);
-  
-          res.json({ success: true });
-        };
-      } catch (parseError) {
-        console.error(parseError);
-        res.status(500).json({ error: 'Error parsing JSON' });
+    const items = JSON.parse(data).items;
+
+    const item = items.find(item => item.id === id)
+
+    if (item) {
+      res.json(item);
+    } else {
+      res.status(404).json({ error: 'Datensatz nicht gefunden.' });
       }
     });
   });
+
+app.delete('/records/:id', (req, res) => {
+  const id = req.params.id;
+
+  fs.readFile('src/records.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: 'Server-Fehler.' });
+    }
+  
+  const items = JSON.parse(data).items;
+
+  const item = items.findIndex(item => item.id === id)
+
+    if (item !== -1) {
+      items.splice(item, 1);
+      data.items = items
+      fs.writeFile('src/records.json', JSON.stringify(data), 'utf8', err => { 
+        if (err) {
+          res.status(500).json({ error: 'Server-Fehler.' });
+        } else {
+          res.status(204).send("Power record deleted successfully.");
+        }
+      });
+    } else {
+      res.status(404).json('The power record with the given ID does not exist.')
+    }
+  });
+}); 
