2cee34ca9e25c8a5055e7deae69a39ffb3f60362
S1mon1609 <116732154+S1mon1609@users.noreply.github.com>
2023-06-05 16:01:21 +0200

    1 file modified

--- src/server.js
+++ src/server.js
@@ -29,13 +29,16 @@ function validateRecord(record) {
 }
 
 app.get('/records', (req, res) => {
+  // read of the existing JSON-File
     fs.readFile('src/records.json', 'utf8', (err, data) => {
+      // check that file has been read successfully
       if (err) {
         console.error(err);
         res.status(500).send();
         return;
       }
-  
+      
+      //Parse-JSON, send parsed JSON as the respons, in cas parse is unsuccsessfull send 500 response
       try {
         const records = JSON.parse(data);
         res.json(records);
@@ -46,94 +49,111 @@ app.get('/records', (req, res) => {
     });
   });
   
-  app.post('/records', (req, res) => {
-    const newRecord = req.body;
+app.post('/records', (req, res) => {
+  // save given record as variable
+  const newRecord = req.body;
   
-    if (!validateRecord(newRecord)) {
-      res.status(400).send();
+  // validate that record has all and only the required data
+  if (!validateRecord(newRecord)) {
+    res.status(400).send();
+    return;
+  }
+  // read of the existing JSON-File
+  fs.readFile('src/records.json', 'utf8', (err, data) => {
+    // check that file has been read successfully
+    if (err) {
+      console.error(err);
+      res.status(500).send();
+      return;
+      //given record receives an id and is added to the parsed JSON document
+    } else {
+      const jsonData = JSON.parse(data);
+      let newItem = req.body;
+      const id = generateId()
+      newItem.id = id;
+  
+      jsonData.items.push(newRecord);
+      
+      // JSON with new record is wrote back into the json file. If stringify or write fails a 500 response is given. If it is successfull a response is given that includes a header with the received id of the new record.
+      fs.writeFile('src/records.json', JSON.stringify(jsonData, null, 2), 'utf8', (err) => {
+        if (err) {
+          console.error(err);
+          res.status(500).send();
+          return;
+        } else {
+          const header = getRecordUrl(req, id);
+          res.status(201).location(header);
+        }
+      });
+    }
+  });
+});
+  
+app.delete('/records', (req, res) => {
+
+  //empty JSON is created as a variable
+  const emptyRecord = {items: []} 
+
+  //empty JSON is wrote into the existing JSON document and replaces all existing records. If writte or stringify are unsuccessfull a 500 response is given. If everything is successfull a 204 response is given confirming the delete.
+  fs.writeFile('src/records.json', JSON.stringify(emptyRecord, null, 2), 'utf8', (err) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Internal Server Error' });
       return;
     }
-  
-    fs.readFile('src/records.json', 'utf8', (err, data) => {
-      if (err) {
-        console.error(err);
-        res.status(500).send();
-        return;
-      } else {
-        const jsonData = JSON.parse(data);
-        let newItem = req.body;
-        const id = generateId()
-        newItem.id = id;
-  
-        jsonData.items.push(newRecord);
-  
-        fs.writeFile('src/records.json', JSON.stringify(jsonData, null, 2), 'utf8', (err) => {
-          if (err) {
-            console.error(err);
-            res.status(500).send();
-            return;
-          } else {
-            const header = getRecordUrl(req, id);
-            res.status(201).location(header);
-          }
-        });
-
-      }
-    });
-  });
-  
-  app.delete('/records', (req, res) => {
-
-    const emptyRecord = {items: []} 
-
-    fs.writeFile('src/records.json', JSON.stringify(emptyRecord, null, 2), 'utf8', (err) => {
-      if (err) {
-        console.error(err);
-        res.status(500).json({ error: 'Internal Server Error' });
-        return;
-      }
       
-      res.status(204).send("All power records deleted succesfully.");
+    res.status(204).send("All power records deleted succesfully.");
   
     
-    });
   });
+});
 
-  app.get('/records/:id', (req, res) => {
-
-    const id = req.params.id;
-
-    fs.readFile('src/records.json', 'utf8', (err, data) => {
-      if (err) {
-        console.error(err);
-        return res.status(500).send();
-      }
-  
-    const items = JSON.parse(data).items;
-
-    const item = items.find(item => item.id === id)
-
-    if (item) {
-      res.json(item);
-    } else {
-      res.status(404).send();
-      }
-    });
-  });
-
-app.delete('/records/:id', (req, res) => {
+app.get('/records/:id', (req, res) => {
+  // id given in request is saved.
   const id = req.params.id;
 
+  // read of the existing JSON-File
   fs.readFile('src/records.json', 'utf8', (err, data) => {
+    // check that file has been read successfully
+    if (err) {
+      console.error(err);
+      return res.status(500).send();
+    }
+  
+  //Parsed JSON is saved as a constant
+  const items = JSON.parse(data).items;
+
+  //search object with given id within the JSON
+  const item = items.find(item => item.id === id)
+
+  //If id is found record with id is in a 200 response. If record is not found a 404 response is given.
+  if (item) {
+    res.json(item);
+  } else {
+    res.status(404).send();
+    }
+  });
+});
+
+app.delete('/records/:id', (req, res) => {
+  // id given in request is saved.
+  const id = req.params.id;
+
+  // read of the existing JSON-File
+  fs.readFile('src/records.json', 'utf8', (err, data) => {
+    // check that file has been read successfully
     if (err) {
       console.error(err);
       return res.status(500).send();
     } else {
   
+    //save JSON as variable  
     let jsonData = JSON.parse(data);
 
+    // find record with given element
     const index = jsonData.items.findIndex(item => item.id === id);
 
+    // remove found record an write all records without the removed one back into the JSON document. If unsuccsessfull 500 response is given. If successfull 204 response is given confirming deletion. If no record with the given id is found 404 error is given.
     if (index !== -1) {
       jsonData.items.splice(index, 1);
       fs.writeFile('src/records.json', JSON.stringify(jsonData, null, 2), 'utf8', err => {
